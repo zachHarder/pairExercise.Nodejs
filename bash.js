13a100ba6019859cc14a1a1710074b034736ccf6
Jeff Lupton <jeffreylupton123@gmail.com>
2022-03-04 12:12:02 -0800

    implements recursive node refresh

---
+++ bash.js
@@ -0,0 +1,11 @@
+process.stdout.write("prompt > ");
+
+process.stdin.on("data", (data) => {
+  const cmd = data.toString().trim();
+  if (cmd === "pwd") {
+    process.stdout.write(process.cwd());
+  } else {
+    process.stdout.write("You type: " + cmd);
+    process.stdout.write("\nprompt > ");
+  }
+});
